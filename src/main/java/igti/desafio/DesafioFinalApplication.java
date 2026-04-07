a5ded1317e6a98892084dc1954c398a0b21a20bb
EvertonTrennepohl <gauchotp@gmail.com>
2026-04-07 20:50:20 -0300

    Preparando para atualizar Heroku

--- src/main/java/igti/desafio/DesafioFinalApplication.java
+++ src/main/java/igti/desafio/DesafioFinalApplication.java
@@ -2,7 +2,6 @@ package igti.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
