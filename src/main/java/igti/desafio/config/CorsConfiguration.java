3d64f6d3d2068cdc95daf297baf4bceb9de26391
EvertonTrennepohl <gauchotp@gmail.com>
2026-05-27 23:56:01 -0300

    Ativa allowCredentials no CORS para aceitar tokens do Angular

--- src/main/java/igti/desafio/config/CorsConfiguration.java
+++ src/main/java/igti/desafio/config/CorsConfiguration.java
@@ -13,7 +13,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedOrigins("https://ngfood.vercel.app")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+            .allowCredentials(true);
     }
     
     @Bean
